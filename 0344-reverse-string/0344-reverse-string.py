fb060979d7c7a4a0dd08e5f45c6d9494af062d95
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2026-05-21 00:31:15 +0900

    Time: 0 ms (100%), Space: 23.3 MB (97.04%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,5 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
+        # s.reverse()
+        
+        # two-pointer
+        left,right = 0, len(s)-1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left +=1 
+            right-=1
+
         
