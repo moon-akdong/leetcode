3c1820bb3a4dba3deafe2f35b0864573d7286e77
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2026-05-24 00:37:47 +0900

    Time: 7 ms (74.69%), Space: 21.4 MB (7.61%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,18 +1,33 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left,right = 0, len(height)-1
-        left_max, right_max = height[left],height[right]
-        water = 0
-        while left < right:
-            left_max, right_max = max(height[left],left_max), \
-                                    max(height[right],right_max)
-            if left_max <= right_max:
-                water += left_max - height[left]
-                left +=1 
-            elif left_max > right_max:
-                water += right_max - height[right]
-                right -=1 
-        return water
-            
+        # left,right = 0, len(height)-1
+        # left_max, right_max = height[left],height[right]
+        # water = 0
+        # while left < right:
+        #     left_max, right_max = max(height[left],left_max), \
+        #                             max(height[right],right_max)
+        #     if left_max <= right_max:
+        #         water += left_max - height[left]
+        #         left +=1 
+        #     elif left_max > right_max:
+        #         water += right_max - height[right]
+        #         right -=1 
+        # return water
+        
+        # Stack 
+        stack = [] 
+        volume = 0 
 
-            
+        for i in range(len(height)):
+            # 변곡점을 만나는 경우 
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+
+                if not len(stack):
+                    break 
+                
+                distance = i - stack[-1] -1
+                waters = min(height[i], height[stack[-1]])-height[top]
+                volume += distance * waters 
+            stack.append(i)
+        return volume
