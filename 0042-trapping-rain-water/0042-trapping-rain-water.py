bbca2efa3ccd13468ff17c9568d23b29402c9122
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2026-06-07 18:28:32 +0900

    Time: 11 ms (45.98%), Space: 21.1 MB (49.31%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,33 +1,23 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # left,right = 0, len(height)-1
-        # left_max, right_max = height[left],height[right]
-        # water = 0
-        # while left < right:
-        #     left_max, right_max = max(height[left],left_max), \
-        #                             max(height[right],right_max)
-        #     if left_max <= right_max:
-        #         water += left_max - height[left]
-        #         left +=1 
-        #     elif left_max > right_max:
-        #         water += right_max - height[right]
-        #         right -=1 
-        # return water
+        volume = 0
+        if not height:
+            return volume 
         
-        # Stack 
-        stack = [] 
-        volume = 0 
+        left,right = 0, len(height)-1 
+        left_max, right_max = height[left], height[right]
 
-        for i in range(len(height)):
-            # 변곡점을 만나는 경우 
-            while stack and height[i] > height[stack[-1]]:
-                top = stack.pop()
-
-                if not len(stack):
-                    break 
-                
-                distance = i - stack[-1] -1
-                waters = min(height[i], height[stack[-1]])-height[top]
-                volume += distance * waters 
-            stack.append(i)
+        while left < right:
+            left_max = max(left_max, height[left])
+            right_max = max(right_max,height[right])
+        
+            if left_max < right_max: 
+                volume += left_max - height[left]
+                left +=1 
+            elif left_max >= right_max:
+                volume += right_max - height[right]
+                right-=1 
         return volume
+
+        
+
