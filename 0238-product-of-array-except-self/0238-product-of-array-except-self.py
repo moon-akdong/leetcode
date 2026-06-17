722f578055e07fdd8e38cc5f0cd95c22d9ded971
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2026-06-18 06:02:17 +0900

    Time: 21 ms (61.37%), Space: 25.7 MB (45.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,26 +1,26 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # out = [] 
-        # p = 1 
-        # for i in range(0,len(nums)):
-        #     out.append(p)
-        #     p = p*nums[i]
-        # p = 1
-        # for i in range(len(nums)-1,0-1,-1):
-        #     out[i] = out[i] * p
-        #     p = p* nums[i]
-        # return out
-        def element_product(nums,sub=1):
-            for e in nums:
-                yield sub
-                sub = sub * e
+        out = [] 
+        p = 1 
+        for i in range(0,len(nums)):
+            out.append(p)
+            p = p*nums[i]
+        p = 1
+        for i in range(len(nums)-1,0-1,-1):
+            out[i] = out[i] * p
+            p = p* nums[i]
+        return out
+        # def element_product(nums,sub=1):
+        #     for e in nums:
+        #         yield sub
+        #         sub = sub * e
                 
-        it = element_product(nums)
-        it2= element_product(nums[::-1])
+        # it = element_product(nums)
+        # it2= element_product(nums[::-1])
        
-        ls = list(it)
-        ls2 = list(it2)[::-1]
-        result = [] 
-        for i in range(len(ls)):
-            result.append(ls[i] * ls2[i])
-        return result
+        # ls = list(it)
+        # ls2 = list(it2)[::-1]
+        # result = [] 
+        # for i in range(len(ls)):
+        #     result.append(ls[i] * ls2[i])
+        # return result
