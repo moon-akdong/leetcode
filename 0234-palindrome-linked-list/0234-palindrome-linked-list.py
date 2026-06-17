062f46214db6d94e645626eaaf250da362e40f7f
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2026-06-18 06:48:08 +0900

    Time: 28 ms (71.61%), Space: 53.4 MB (36.18%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # 리스트 변환 
+        q = collections.deque()
+        if not head:
+            return True 
+        
+        node = head 
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+
+        # deque를 이용한 최적화 
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+                return False 
+        return True
+        # 러너를 이용한 풀이
