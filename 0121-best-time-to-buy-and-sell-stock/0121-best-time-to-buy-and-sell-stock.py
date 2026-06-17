be0668c57530403f01993115d80a3fdce543fd31
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2026-06-18 06:18:51 +0900

    Time: 59 ms (34.49%), Space: 28.8 MB (11.95%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -2,6 +2,9 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_num = sys.maxsize
         profit = -sys.maxsize
+        if not profit:
+            return 0 
+            
         for p in prices:
             min_num = min(p,min_num)
             profit = max(profit, p - min_num)
