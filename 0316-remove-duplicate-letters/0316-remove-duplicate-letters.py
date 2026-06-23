a1374224ad04b6d0464dd29cbebc8510843cfd7c
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2026-06-24 00:00:03 +0900

    Time: 4 ms (26.59%), Space: 19.6 MB (14.73%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -5,7 +5,7 @@ class Solution:
 
         for char in s:
             counter[char] -= 1 
-            if char in seen:
+            if char in stack:
                 continue
             while stack and char < stack[-1] and \
                     counter[stack[-1]] > 0:
