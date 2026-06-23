e1b46f46ebd41c79daab71785d43924c326ffa93
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2026-06-23 23:36:18 +0900

    Time: 0 ms (100%), Space: 18.9 MB (99.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,9 +8,9 @@ class Solution:
         stack = [] 
 
         for i in s:
-            if i not in table:
+            if not stack or i not in table:
                 stack.append(i)
             
-            elif not stack or table[i] != stack.pop():
+            elif table[i] != stack.pop():
                 return False
         return len(stack) == 0
